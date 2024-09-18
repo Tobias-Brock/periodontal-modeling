ce32c8cab167b9e8e576db002ca23b0cf384e68b
Tobias-Brock <t.brock335@googlemail.com>
2024-09-18 19:31:58 +0200

    update metric calculation

--- pamod/resampling/__init__.py
+++ pamod/resampling/__init__.py
@@ -1,6 +1,6 @@
 """Package provides resampling techniques and metric evaluation."""
 
-from pamod.resampling._metrics import MetricEvaluator, brier_loss_multi
+from pamod.resampling._metrics import MetricEvaluator, brier_loss_multi, get_probs
 from pamod.resampling._resampler import Resampler
 
-__all__ = ["MetricEvaluator", "Resampler", "brier_loss_multi"]
+__all__ = ["MetricEvaluator", "Resampler", "brier_loss_multi", "get_probs"]
--- pamod/tuning/_basetuner.py
+++ pamod/tuning/_basetuner.py
@@ -1,10 +1,25 @@
+from abc import ABC, abstractmethod
 from typing import Dict, Optional, Union
 
 import numpy as np
 
 from pamod.base import BaseEvaluator
+from pamod.resampling import MetricEvaluator
 from pamod.training import Trainer
-from pamod.tuning._thresholdopt import ThresholdOptimizer
+
+
+class MetaTuner(ABC):
+    """Abstract base class enforcing implementation of tuning strategies."""
+
+    @abstractmethod
+    def cv(self, *args, **kwargs):
+        """Perform cross-validation based tuning."""
+        pass
+
+    @abstractmethod
+    def holdout(self, *args, **kwargs):
+        """Perform holdout based tuning."""
+        pass
 
 
 class BaseTuner(BaseEvaluator):
@@ -22,10 +37,10 @@ class BaseTuner(BaseEvaluator):
             hpo (str): The hyperparameter optimization method.
         """
         super().__init__(classification, criterion, tuning, hpo)
-        self.trainer = Trainer(self.classification, self.criterion, self.tuning)
-        self.threshold_optimizer = ThresholdOptimizer(
-            self.classification, self.criterion
+        self.trainer = Trainer(
+            self.classification, self.criterion, self.tuning, self.hpo
         )
+        self.metric_evaluator = MetricEvaluator(self.classification, self.criterion)
 
     def _print_iteration_info(
         self,
@@ -47,8 +62,21 @@ class BaseTuner(BaseEvaluator):
                 (for binary classification).
         """
         model_name = model.__class__.__name__
-        params_str = ", ".join([f"{key}={value}" for key, value in params_dict.items()])
-        score_value = score if np.isscalar(score) else None
+        params_str = ", ".join(
+            [
+                (
+                    f"{key}={value:.4f}"
+                    if isinstance(value, (int, float))
+                    else f"{key}={value}"
+                )
+                for key, value in params_dict.items()
+            ]
+        )
+        score_value = (
+            f"{score:.4f}"
+            if np.isscalar(score) and isinstance(score, (int, float))
+            else None
+        )
 
         if self.tuning == "holdout":
             print(
